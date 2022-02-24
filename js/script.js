6743d79d5ea5949e578a948977dbd03ef413775c
Julick Mellah <74454432+Melliche@users.noreply.github.com>
2022-02-24 22:51:39 +0100

    turn

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 let playerstart;
 let tour = 0
 // let i = 1
-isgreen = false
 let idclick;
-
+let listcase = [];
+let idrdm;
+let botstart = false;
+let botturn = false;
 for (let i = 1; i < 10; i++){
     console.log(i)
     numero = document.createElement("div");
@@ -14,12 +16,10 @@ for (let i = 1; i < 10; i++){
         br = document.createElement("br");
         document.getElementById("grille").appendChild(br);
     }
-    if (i > 9) {
-        clearTimeout(time);
+    if (i >= 9) {
         starter();
     }
 } 
-
 
 let onClick = function click() {
     console.log(this.id);
@@ -32,46 +32,60 @@ for (let i = 1; i < 10; i++){
     document.getElementById(i).onclick = onClick;
 }
 
-
-
-
 function starter() {
 	let tour1 = Math.floor(Math.random() * 2 + 1);
 	playerstart = tour1;
+    console.log(playerstart)
 	start();
 }
 
 function start () {
     if (playerstart == 1){
-        startplayer();
+        // startplayer();
     }else{
+        botstart = true;
         startbot();
     }
 }
 
 function pair (nbr){
     if (nbr%2 == 0){
-
     }
 }
 
 function startplayer () {
-    console.log("ZE")
+    tour++;
+    console.log("eben")
+    listcase.push(idclick);
     document.getElementById(idclick).style.backgroundColor = "blue";
-    
+    botturn = true;
+    console.log(botturn)
+    startbot();
   
 }
 
 function startbot () {
-    if (playerstart != 1){
+    if (playerstart != 1 && tour < 2){
         tour++;
-        idrdm = Math.floor(Math.random() * 9 + 1)
-        document.getElementById(idrdm).style.backgroundColor = "red";
-        startplayer();
-        
+        let idrdm = Math.floor(Math.random() * 9 + 1)
+        listcase.push(idrdm);  
+        document.getElementById(idrdm).style.backgroundColor = "red"; 
+        console.log("START")
+        console.log(idrdm) 
     }
+    if (botstart == false || botturn == true ){
+        console.log(botturn)
+         while (idrdm == listcase[1] || idrdm == listcase[2] || idrdm == listcase[3] || idrdm == listcase[4] || idrdm == listcase[5] || idrdm == listcase[6] || idrdm == listcase[7] || idrdm == listcase[8] || idrdm == listcase[9] || idrdm == listcase[0]){
+            console.log(botturn)
+            idrdm = Math.floor(Math.random() * 9 + 1)
+            console.log(idrdm) 
+        }
+        document.getElementById(idrdm).style.backgroundColor = "red"; 
+        botturn = false;
+    }
+   
+    
 
-    startplayer();
 }
 
 
